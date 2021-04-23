d24a8bdf10825691be9db28f7d0dbb16a8e9114e
JPig-Main <jumpei141107@gmail.com>
2021-04-23 15:18:40 +0900

    Edit: centering button

--- src/components/Rollback/Roleback.js
+++ src/components/Rollback/Roleback.js
@@ -2,32 +2,37 @@ import XIcon from "../../resources/x";
 import CheckIcon from "../../resources/check";
 import React from "react";
 
-export default class extends React.Component{
-    constructor (props) {
+export default class extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {
-            Icon: XIcon,
-            message: "点呼する"
+        this.displayList = {
+            notSubmitted: {
+                Icon: XIcon,
+                message: "点呼する",
+                bgColor: "bg-red-300"
+            },
+            submitted: {
+                Icon: CheckIcon,
+                message: "点呼完了",
+                bgColor: "bg-green-300"
+            }
         }
+        this.state = { display: this.displayList.notSubmitted }
     }
 
     submit = () => {
-        // バックエンド処理
-        this.setState({
-            Icon: CheckIcon,
-            message : "点呼完了"
-        })
+        this.setState({ display: this.displayList.submitted })
     }
 
     render() {
         return (
-            <div className="py-auto">
-                <div className="text-xl">0001号室</div>
-                <div className="px-auto py-auto h-full">
-                    <button className="mx-auto w-48 h-48 my-auto border border-2 shadow-xl rounded-full" onClick={() => this.submit()}>
-                        <this.state.Icon className="w-20 h-20 mx-auto" />
-                        {this.state.message}
+            <div className="flex justify-center items-center h-screen">
+                <div>
+                    <button className={`${this.state.display.bgColor} focus:outline-none md:w-64 md:h-64 w-40 h-40 border-2 shadow-xl rounded-full text-white text-sm md:text-xl`} onClick={() => this.submit()}>
+                        <this.state.display.Icon className="w-20 h-20 mx-auto" />
+                        {this.state.display.message}
                     </button>
+                    <div className="text-base mt-2 md:text-2xl md:mt-3">0001号室</div>
                 </div>
             </div>
         )
