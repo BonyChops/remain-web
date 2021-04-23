506e1cb369e17e065fde55cc14ed03152e5306d3
JPig-Main <jumpei141107@gmail.com>
2021-04-23 16:33:19 +0900

    Edit: change room from roomId

--- src/components/History/parts/Table.js
+++ src/components/History/parts/Table.js
@@ -4,7 +4,7 @@ const Table = (props) => (
     <div className="border-t-2 border-b-2 text-sm md:text-lg mx-auto w-72 md:w-96 mt-5 md:mt-10">
         {props.rows.map(row => (
             <div className="tr px-3 md:px-6 py-1 md:py-2 flex">
-                <div className="flex w-18">{`${props.hall}${props.floor}${row.roomId.toString().padStart(2, '0')}号室`}</div>
+                <div className="flex w-18">{`${props.hall}${props.floor}${row.room.toString().padStart(2, '0')}号室`}</div>
                 <div className="flex w-18 pl-3 md:pl-6">{row.name}</div>
                 <div className="flex-1 flex pl-3 md:pl-6">{row.result}</div>
             </div>
