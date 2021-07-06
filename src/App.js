e055c239148d559df23291b4b4a9eec7d691334e
BonyChops <contact.bonychops@gmail.com>
2021-07-06 18:11:57 +0900

    Edit: before delete

--- src/App.js
+++ src/App.js
@@ -3,23 +3,50 @@ import Footer from "./components/Footer/Footer";
 import Rollback from "./components/Rollback/Rollback";
 import History from "./components/History/History";
 import UnSubmit from "./components/UnSubmit/UnSubmit";
-import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
+import React from 'react';
+import {BrowserRouter, Route, Switch, withRouter} from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
-function App(props) {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={"/"} render={() => <Rollback />} />
-          <Route exact path={"/history"} render={() => <History />} />
-          <Route exact path={"/unSubmit"} render={() => <UnSubmit />} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer isSubmit={false} unSubmitNum={1} />
-      </BrowserRouter>
-    </div>
-  );
+const publicIp = require('public-ip');
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ready: false
+        }
+    }
+
+    MySwal = withReactContent(Swal)
+
+    componentDidMount() {
+        this.MySwal.fire({title: "しばらくお待ちください..."})
+        this.MySwal.showLoading();
+        publicIp.v4().then((ip) => {
+            this.MySwal.close();
+            this.MySwal.fire(ip);
+            this.setState({ready: true})
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path={"/"} render={() => <Rollback state={this.state}/>}/>
+                        <Route exact path={"/history"} render={() => <History state={this.state}/>}/>
+                        <Route exact path={"/unSubmit"} render={() => <UnSubmit state={this.state}/>}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                    <Footer isSubmit={false} unSubmitNum={1} state={this.state}/>
+                </BrowserRouter>
+            </div>
+        );
+    }
+
 }
 
 export default App;
--- src/components/Footer/parts/FooterButton.js
+++ src/components/Footer/parts/FooterButton.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 const FooterButton = (props) => {
     return (
-        <Link className={`relative mx-auto ${props.isCurrent ? 'text-gray-700' : 'text-gray-400'} `} to={props.to}>
-            <props.icon className={`w-10 h-10 mx-2 `} />
-            {props.name}
-            {props.to === "/unSubmit" && (
+        <Link className={`relative mx-auto ${props.isCurrent && props.state.ready ? 'text-gray-700' : 'text-gray-400'} `} to={props.to}>
+            <props.icon className={`w-10 h-10 mx-2 ` + !props.state.ready ? "animate-pulse" : ""} />
+            {props.state.ready ? props.name : <div className="w-12 h-4 bg-gray-400 rounded-xl animate-pulse mx-auto"/> }
+            {props.unSubmitNum !== undefined && (
                 <div className="z-10 absolute top-0 right-0 w-5 h-5 text-sm rounded-full text-gray-100 bg-red-400">{props.unSubmitNum}</div>
             )}
         </Link>
